a13a9a4314f0308b0e58da5a23c6b859cce07845
Suliman Fayyaz <hydrajin@gmail.com>
2022-01-18 22:12:25 -0500

    add interactivity to the button

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,13 +4,22 @@ import "components/Button.scss";
 
 export default function Button(props) {
    let buttonClass = "button";
-
+ 
    if (props.confirm) {
-      buttonClass += " button--confirm";
+     buttonClass += " button--confirm";
    }
+ 
    if (props.danger) {
-      buttonClass += " button--danger";
+     buttonClass += " button--danger";
    }
-
-   return <button className={buttonClass}>{props.children}</button>;
-}
+ 
+   return (
+     <button
+       className={buttonClass}
+       onClick={props.onClick}
+       disabled={props.disabled}
+     >
+       {props.children}
+     </button>
+   );
+ }
