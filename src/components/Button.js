38f6c1098358dae58d22b3e6e27261bfe24befb1
Suliman Fayyaz <hydrajin@gmail.com>
2022-01-18 21:19:16 -0500

    fix typo children in button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,6 +8,9 @@ export default function Button(props) {
    if (props.confirm) {
       buttonClass += " button--confirm";
    }
+   if (props.danger) {
+      buttonClass += " button--danger";
+   }
 
-   return <button className={buttonClass}>{props.childern}</button>;
+   return <button className={buttonClass}>{props.children}</button>;
 }
