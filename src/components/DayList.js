e1f1e2340be77ac30f23d59ae4883d5a7308984f
Suliman Fayyaz <hydrajin@gmail.com>
2022-01-19 15:19:29 -0500

    pass data to each DayListItem with .map()

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,9 +2,22 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
+  const { days } = props;
+  const parsedDays = days.map((day) =>
+  // Each object in the days array contains all the information to populate one <DayListItem> component.
+  // So, we can iterate over the days array and use its data to create a new array of <DayListItem> components which can be rendered in the <ul> of our <DayList>.
+  // To achieve this, it is recommended to use the .map method. 
+    <DayListItem
+    key={day.id}
+    name={day.name}
+    spots={day.spots}
+    selected={day.name === props.day}
+    setDay={day.setDay}
+    />
+  );
   return (
     <ul>
-      
+      {parsedDays}
     </ul>
-  )
+  );
 }
