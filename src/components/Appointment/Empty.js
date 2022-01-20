1c5abfd002f6659e608a3e8d6a6d4af9bf38b9d3
Suliman Fayyaz <hydrajin@gmail.com>
2022-01-20 18:20:04 -0500

    add Header with time props in index (Gary J) and fixed onClick placement for onEdit/onDelete

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -2,9 +2,10 @@ import React from "react";
 
 export default function Empty(props) {
   return(
-    <main className="appointment__add" onClick={props.onAdd}>
+    <main className="appointment__add">
       <img
         className="appointment__add-button"
+        onClick={props.onAdd}
         src="images/add.png"
         alt="Add"
       />
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Header from "./Header";
 import "./styles.scss";
 
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+    </article>
   );
 }
