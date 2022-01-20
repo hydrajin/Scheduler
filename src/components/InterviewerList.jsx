6824e267ac0e08b4007d7c3a5c75add5ba35da40
Suliman Fayyaz <hydrajin@gmail.com>
2022-01-19 22:45:57 -0500

    refactor InterviewerList

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
+/* interviewers:array - an array of objects as seen above
+setInterviewer:function - a function that accepts an interviewer id. This function will simply be passed down to the InterviewerListItem
+interviewer:number - a number that represents the id of the currently selected interviewer */
 
 export default function InterviewerList(props) {
+  const { interviewers } = props;
+  const parsedInterviewers = interviewers.map((interviewer) => (
+    // No need for return after .map()
+    <InterviewerListItem
+      key={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={interviewer.id === props.interviewer}
+      setInterviewer={() => props.setInterviewer(interviewer.id)}
+      // function accepts interviewer id
+    />
+  ));
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{parsedInterviewers}</ul>
     </section>
   );
 }
