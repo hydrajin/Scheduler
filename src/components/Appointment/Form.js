fcf264ce6bdbcbafa3feee79dab62bf45fa8ac45
Suliman Fayyaz <hydrajin@gmail.com>
2022-01-21 02:33:31 -0500

    fix input field reset bug + onSubmit bug

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -8,30 +8,47 @@ export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
+  console.log(props.student);
+
+  const reset = () => {
+    setStudent("");
+    setInterviewer("");
+  }
+  console.log(reset);
+  // When a user clicks the Cancel button, we use this function to clear the form values
+
+  const cancel = () => {
+    props.onCancel();
+    reset();
+  }
+  //! Input field reset bug?
+  // Add a cancel function to the Form component that calls reset() and props.onCancel.
+
   return(
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
             onChange={(event) => setStudent(event.target.value)}
-            value={props.student}
+            value={student}
           />
         </form>
         <InterviewerList 
           interviewers={props.interviewers}
-          value={props.interviewers}
+          value={interviewer}
           onChange={setInterviewer}
           // from refactored controlled lists walkthrough
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          {/* When cancel is pressed, the above cancel button is run */}
+          <Button onClick={() => props.onSave(student, interviewer)} >Save</Button>
         </section>
       </section>
     </main>
