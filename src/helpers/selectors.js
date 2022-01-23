597063170011cd4bead15ae2a6a5f97a085fa11a
Suliman Fayyaz <hydrajin@gmail.com>
2022-01-22 20:46:02 -0500

    passing tests for data with selectors

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,57 +1,49 @@
-import { getAppointmentsForDay } from "helpers/selectors";
+/* We don't always have control over the format of data returned by an API. 
+Sometimes we must transform the data before we use it with components. 
+When we make a request to /api/appointments we retrieve an object with all of the appointments,
+but to show the schedule for a particular day, we need to provide it an 
+array of appointments for that day. */
 
-const state = {
-  days: [
-    {
-      id: 1,
-      name: "Monday",
-      appointments: [1, 2, 3]
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      appointments: [4, 5]
-    }
-  ],
-  appointments: {
-    "1": { id: 1, time: "12pm", interview: null },
-    "2": { id: 2, time: "1pm", interview: null },
-    "3": {
-      id: 3,
-      time: "2pm",
-      interview: { student: "Archie Cohen", interviewer: 2 }
-    },
-    "4": { id: 4, time: "3pm", interview: null },
-    "5": {
-      id: 5,
-      time: "4pm",
-      interview: { student: "Chad Takahashi", interviewer: 2 }
-    }
+/* Selectors
+We are going to create a function called getAppointmentsForDay that will receive two 
+arguments state and day. The function will return an array of appointments for the given day.
+
+There is often a need to compute new data from existing state in an application. 
+To do this we can use a selector, a function that accepts state as an argument and returns 
+data that is derived from that state.
+
+EXAMPLE
+function selectUserByName(state, name) {
+  const filteredNames = state.users.filter(user => user.name === name);
+  return filteredNames;
+} */
+
+export function getAppointmentsForDay(state, day) {
+  const filteredDays = state.days.filter(dayArr => dayArr.name === day); 
+  // console.log(filteredDays); // -> [ { id: 1, name: 'Monday', appointments: [ 1, 2, 3 ] } ]
+  // console.log(filteredDays[0]); // -> { id: 1, name: 'Monday', appointments: [ 1, 2, 3 ] }
+  // console.log(filteredDays[0].appointments) // -> [ 1, 2, 3 ] <- (appointments)
+
+  if (filteredDays.length === 0) {
+    // ✓ getAppointmentsForDay returns an array 
+    // ✓ getAppointmentsForDay returns an empty array when the days data is empty
+    // ✓ getAppointmentsForDay returns an empty array when the day is not found
+    return [];
   }
-};
-
-test("getAppointmentsForDay returns an array", () => {
-  const result = getAppointmentsForDay(state, "Monday");
-  expect(Array.isArray(result)).toBe(true);
-});
-
-test("getAppointmentsForDay returns an array with a length matching the number of appointments for that day", () => {
-  const result = getAppointmentsForDay(state, "Monday");
-  expect(result.length).toEqual(3);
-});
-
-test("getAppointmentsForDay returns an array containing the correct appointment objects", () => {
-  const [first, second] = getAppointmentsForDay(state, "Tuesday");
-  expect(first).toEqual(state.appointments["4"]);
-  expect(second).toEqual(state.appointments["5"]);
-});
-
-test("getAppointmentsForDay returns an empty array when the days data is empty", () => {
-  const result = getAppointmentsForDay({ days: [] }, "Monday");
-  expect(result.length).toEqual(0);
-});
-
-test("getAppointmentsForDay returns an empty array when the day is not found", () => {
-  const result = getAppointmentsForDay(state, "Wednesday");
-  expect(result.length).toEqual(0);
-});
+  //console.log(filteredDays[0].appointments)
+  const appointmentsArr = filteredDays[0].appointments;
+  const numOfAppointments = []
+  appointmentsArr.forEach(element => numOfAppointments.push(state.appointments[element]))
+  console.log(numOfAppointments);
+  return numOfAppointments;
+}
+/* [
+      { id: 1, time: '12pm', interview: null },
+      { id: 2, time: '1pm', interview: null },
+      {
+        id: 3,
+        time: '2pm',
+        interview: { student: 'Archie Cohen', interviewer: 2 }
+      }
+    ]
+*/
