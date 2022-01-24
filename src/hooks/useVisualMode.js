be79cb67b8acc7c266daead8201cce6a1fa1d4a3
Suliman Fayyaz <hydrajin@gmail.com>
2022-01-24 17:33:53 -0500

    finish building a hook with passing tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,18 +1,32 @@
-import { useState } from "react";
-/* To pass the first test our useVisualMode Hook will need to:
-
-take in an initial mode
-set the mode state with the initial mode provided
-return an object with a mode property */
+import { useState} from "react";
 
 export default function useVisualMode(initial) {
-  //console.log(initial);
   const [mode, setMode] = useState(initial);
-  //console.log(mode);
-  const transition = (newMode) => {
-    setMode(newMode)
-  }
+  const [history, setHistory] = useState([initial]);
 
-  return { mode, transition };
-};
+  // take in a new mode and update the mode state with the new value
+  const transition = (newMode, replace = false) => {
+    // console.log(replace);
+    if (replace) { 
+      // if true (last test)
+      setMode(newMode);
+    } else {
+      setMode(newMode);
+      // console.log([...history, newMode]) // -> [ 'FIRST', 'SECOND', 'THIRD' ]
+      setHistory([...history, newMode]);
+    }
+  };
 
+  const back = () => {
+    // console.log(history.length); 
+    if (history.length <= 1) {
+      setMode(initial);
+    } else {
+      const newHistory = [...history.slice(0, -1)];
+      // console.log(newHistory);
+      setHistory(newHistory);
+      setMode(newHistory[newHistory.length - 1]);
+    }
+  };
+  return { mode, transition, back };
+}
