59a1a562288ff3d5371460614ce7cfe5d9899f42
Suliman Fayyaz <hydrajin@gmail.com>
2022-01-27 14:57:25 -0500

    add final test to Form to greatly improve coverage (>93.75%)

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -30,7 +30,8 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
-  
+    
+    setError("");
     props.onSave(student, interviewer);
   }
 
