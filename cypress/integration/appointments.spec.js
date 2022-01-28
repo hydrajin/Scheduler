503faafdc200b8b0205fea8249fb4c7cc32f1853
Suliman Fayyaz <hydrajin@gmail.com>
2022-01-27 23:45:13 -0500

    finish writing appointment tests

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,10 +1,16 @@
 describe("Appointments", () => {
-  
-// 1. Within the test function for the first test, visit the root of the web server, and confirm that the DOM contains the text "Monday".
-  it("should book an interview", () => {
-    cy.visit("/");
-    cy.contains("Monday");
+  beforeEach(() => {
+// Add the Cypress function to the beginning of the test function and rerun the tests. -> cy.request("GET", "http://localhost:8001/api/debug/reset");
+    cy.request("GET", "/api/debug/reset");
 
+// 1. Within the test function for the first test, visit the root of the web server, and confirm that the DOM contains the text "Monday".
+    cy.visit("/");
+
+    cy.contains("Monday");
+  });
+
+  it("should book an interview", () => {
+  
 // 2. Add the command to the test that clicks the add button for the empty appointment. Remember that cy.get() allows arguments that match the jQuery API.
     cy.get("[alt=Add]")
     .first()
@@ -18,6 +24,36 @@ describe("Appointments", () => {
 
 // 5. Add the command to click the save button.
     cy.contains("Save").click();
-    
+
+// 6. Add the commands that verify that we show the student and interviewer names within and element that has the ".appointment__card--show" class.
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones"); 
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+    .first().click({ force: true });
+
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+  it("should cancel an interview", () => {
+    cy.get("[alt=Delete]")
+      .first()
+      .click({ force: true });
+
+    cy.contains("Confirm").click();
+
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should("not.exist");
   });
 });
